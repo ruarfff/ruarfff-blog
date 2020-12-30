436efc290815ba15209a2d3730b6546038a39e2b
Ruairí O'Brien <ruairitobrien@gmail.com>
2020-12-30 12:28:06 +0000

    Added post

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,11 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+import './src/styles/global.css';
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/styles/normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/styles/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
